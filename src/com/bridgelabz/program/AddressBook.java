64b968fe8ea353e3bcb7fed3657dc46b7a93c1c7
Kuldeep525 <84696370+Kuldeep525@users.noreply.github.com>
2021-07-19 11:51:23 +0530

    added ability to edit data

--- src/com/bridgelabz/program/AddressBook.java
+++ src/com/bridgelabz/program/AddressBook.java
@@ -15,15 +15,16 @@ public class AddressBook {
 	private String PhoneNumber;
 	ArrayList<String> information = new ArrayList<String>();
 	Scanner sc = new Scanner(System.in);
+
 	
 	  void addressInput(){
-		  information = new ArrayList<String>();
-		  System.out.println("Enter the Data of Address") ;
+		
+		  System.out.println(" Enter the Data of Address ") ;
 		  System.out.println("enter the FirstName");
 		  FirstName = sc.next();
 		  information.add(FirstName);
           System.out.println("enter the lastname");
-		  LastName =  sc.next();
+          LastName =  sc.next();
 		  information.add(LastName);
 		  System.out.println("enter the address"); 
 		  Address = sc.next();
@@ -40,23 +41,51 @@ public class AddressBook {
 		  System.out.println("enter the phonenumber ");
 		  PhoneNumber = sc.next();
 		  information.add(PhoneNumber);
-		
-	   
+		  
+	  }  
+	  void showUserOption() {
+		  System.out.println(" Enter 1 for any update in existing contact");
+		  int input = sc.nextInt();
+		  switch (input ) {
+		  case 1 : 
+ 
+			  changeDetails();
+			  break;
+		  default : 
+			  System.out.println(" enter correct input");
+			  break;	  
+		  }		 
 	  }
+  	  
+	  void changeDetails() {
+		  System.out.println(" Enter the location and updated detail.\n 0.FirstName \n 1.LastName \n 2.Address \n 3.city \n 4.state 5. zip \n 6.Phonenumber   ");
+		  int position = sc.nextInt();
+		  String data = sc.next();	  
+		  information.set(position, data);
+		  System.out.println("--Updated Address ----");
+		  Iterator<String>  itr = information.iterator();
+		  while (itr.hasNext()) {
+			  Object obj = itr.next();
+			  System.out.println(obj);
+		  }		  
+	  }		  
+
 	  void display() {
 		  
-		  System.out.println("Data is added in AddresBook " +" "+information);
+		  System.out.println(information);
+		  
 	  }
+	  
 	
 	  public static void main(String[] args ){
-		  System.out.println("Welcome to the Address book ");
+		  System.out.println("---------------Welcome to the Address book----------------------- ");
 		  AddressBook addressbook = new AddressBook();
 		  addressbook.addressInput();		 
 		  addressbook.display();
-		  System.out.println("Add Another Contact details ");
-		  addressbook.addressInput();
-		  addressbook.display();
-		  
+		  addressbook.showUserOption();
+	    	  
 	  }
 }
 
+
+	
